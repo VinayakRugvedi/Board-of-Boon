ecc543e77980c858c387b439cd2bac2b0958d353
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-13 00:25:26 +0530

    Added basic state and events for board component to start with

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -5,13 +5,33 @@ import './Board.css'
 class Board extends React.Component {
   constructor() {
     super()
+    this.state = {
+      listName : ''
+    }
+  }
+
+  acceptListName(event) {
+    if(event.which === 13) {
+      this.setState({
+        listName : ''
+      })
+    }
+  }
+
+  showName(event) {
+    console.log(event)
+    this.setState({
+      listName : event.target.value
+    })
   }
 
   render() {
     return (
       <div className="boardContainer">
-      <List/>
-      <List/>
+        <input className="addNewList" placeholder=" &#43; Add a New List" value={this.state.listName} onKeyUp={this.acceptListName.bind(this)} onChange={this.showName.bind(this)}/>
+        <div className="listComponents">
+          <List/>
+        </div>
       </div>
     )
   }
