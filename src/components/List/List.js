9a3d2cafcd785787145bdb50558fae2de8c85112
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-13 03:00:36 +0530

    Added the highlights elements to be present on each card

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './List.css'
+import Card from '../Card/Card.js'
 
 class List extends React.Component {
   constructor() {
@@ -18,6 +19,10 @@ class List extends React.Component {
     return (
       <div className="listContainer">
         <div className="deleteThisList" title="Delete this entire List" onClick={this.deleteThisList}>&times;</div>
+        <div className="cardComponents">
+          <Card/>
+          <Card/>
+        </div>
         <div className="addNewCard" title="Add a Card to this List" onClick={this.addNewCard}>&#43;</div>
       </div>
     )
