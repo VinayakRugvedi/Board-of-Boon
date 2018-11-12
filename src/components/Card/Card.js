26ebcfc50b875b57a96d2b33be8cc663b8462fc6
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-13 03:07:29 +0530

    Added the highlights elements to be present on each card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,13 +7,19 @@ class Card extends React.Component {
   }
 
   deleteThisCard() {
-    
+
   }
 
   render() {
     return (
       <div className="cardContainer">
         <div className="deleteThisCard" title="Delete this Card" onClick={this.deleteThisCard}>&times;</div>
+        <div className="highlights">
+          <div className="labelColor highlights-items" title="Assigned Label Color">q</div>
+          <div className="descriptionIfAny highlights-items" title="This Card has a Description">w</div>
+          <div className="dueDate highlights-items" title="The Card's due Date">e</div>
+          <div className="checklistItems highlights-items" title="Checklist Items">r</div>
+        </div>
       </div>
     )
   }
