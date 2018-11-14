dd27aba1e6528a140367834f2f3d3f581f6fce13
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-14 20:16:49 +0530

    Set up the components render structure

--- src/components/Card/CardDetail.js
+++ src/components/Card/CardDetail.js
@@ -11,6 +11,19 @@ class CardDetail extends React.Component {
       <div className="cardModalWindowBG">
         <div className="cardModalWindow">
           <div className="closeIt">&times;</div>
+          <div className="cardNameContainer">
+            <p>Name</p>
+            <input/>
+            <button>S</button>
+          </div>
+          <div className="cardDescriptionContainer">
+            <p>Description</p>
+            <textarea></textarea>
+            <button>S</button>
+          </div>
+          <div className="cardLabelContainer">
+            <p>Label</p>
+          </div>
         </div>
       </div>
     )
