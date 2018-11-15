e6b3b5b97c57bf36caf3367047693ce907501339
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-16 02:54:43 +0530

    Used props and states in the right way and modified code w.r.t lifting the state up concept and added the conditional render

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,7 +6,6 @@ class Card extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      name : this.props.card.name,
       isDisabled : false,
       isHidden : 'none'
     }
@@ -19,7 +18,7 @@ class Card extends React.Component {
 
   deleteThisCard(event) {
     event.stopPropagation()
-    this.props.updateCards(this.props.card)
+    this.props.updateCard(this.props.card)
   }
 
   toggleDisable(event) {
@@ -30,9 +29,9 @@ class Card extends React.Component {
   }
 
   editing(event) {
-    this.setState({
-      name : event.target.value
-    })
+    let cardCopy = this.props.card
+    cardCopy.name = event.target.value
+    this.props.updateCard(cardCopy, false)
   }
 
   finishEditing(event) {
@@ -44,7 +43,6 @@ class Card extends React.Component {
   }
 
   toggleHidden(event) {
-    console.log(event)
     this.state.isHidden
     ? this.setState({
       isHidden : ''
@@ -55,22 +53,34 @@ class Card extends React.Component {
   }
 
   render() {
+    console.log(this.props.card.name, 'cname in cardjs')
     return (
       <React.Fragment>
       <div className="cardContainer" onClick={this.toggleHidden}>
         <div className="deleteThisCard" title="Delete this Card" onClick={this.deleteThisCard}>&times;</div>
         <div className="cardNameHolder">
-          <input placeholder=" &#43; Add Card name" disabled={this.state.isDisabled} onChange={this.editing} onKeyUp={this.finishEditing} value={this.state.name} onClick={ (event) => event.stopPropagation()}/>
+          <input placeholder=" &#43; Add Card name" disabled={this.state.isDisabled} onChange={this.editing} onKeyUp={this.finishEditing} value={this.props.card.name} onClick={ (event) => event.stopPropagation()}/>
           <button className="toggleDisableButtonCard" title="EDIT this Card name" onClick={this.toggleDisable}>&#x270E;</button>
         </div>
         <div className="highlights">
-          <div className="labelColor highlights-items" title="Assigned Label Color - Default : White"></div>
-          <div className="descriptionIfAny highlights-items" title="This Card has a Description">&#9776;</div>
-          <div className="dueDate highlights-items" title="The Card's due Date">Nov 3 2018</div>
-          <div className="checklistItems highlights-items" title="Checklist Items">10/10</div>
+          <div className="labelColor" title="Assigned Label Color" style={ { backgroundColor : this.props.card.label} }>
+          </div>
+          {
+            this.props.card.description.length > 0
+            ? <div className="descriptionIfAny" title="This Card has a Description">&#9776;</div>
+            : ''
+          }
+          {
+            this.props.card.dueDate.length > 0
+            ? <div className="dueDate" title="The Card's due Date">
+                { this.props.card.dueDate }
+              </div>
+            : ''
+          }
+          <div className="checklistItems" title="Checklist Items">10/10</div>
         </div>
       </div>
-      <CardDetail isHidden = {this.state.isHidden} toggleHidden = {this.toggleHidden}/>
+      <CardDetail isHidden = {this.state.isHidden} toggleHidden = {this.toggleHidden} card={this.props.card} updateCard={this.props.updateCard}/>
       </React.Fragment>
     )
   }
