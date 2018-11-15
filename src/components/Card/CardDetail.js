9e41b0c9e2bd06efa5eefccaa5e07490548030f5
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-16 02:56:18 +0530

    Added the states and the event handlers methods

--- src/components/Card/CardDetail.js
+++ src/components/Card/CardDetail.js
@@ -4,11 +4,50 @@ import './CardDetail.css'
 class CardDetail extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      description : this.props.card.description,
+      label : this.props.card.label,
+      dueDate : this.props.card.dueDate
+    }
     this.toggleDisplay = this.toggleDisplay.bind(this)
+    this.finishEditing = this.finishEditing.bind(this)
+    this.editingName = this.editingName.bind(this)
+    this.editingDescription = this.editingDescription.bind(this)
+    this.editingDueDate = this.editingDueDate.bind(this)
   }
 
   toggleDisplay() {
     this.props.toggleHidden()
+  }
+
+  editingName(event) {
+    let cardCopy
+    cardCopy = this.props.card
+    cardCopy.name = event.target.value
+    this.props.updateCard(cardCopy, false)
+  }
+
+  editingDescription(event) {
+    this.setState({
+      description : event.target.value
+    })
+  }
+
+  editingDueDate(event) {
+    this.setState({
+      dueDate : event.target.value
+    })
+  }
+
+  finishEditing(event) {
+    let cardCopy
+      cardCopy = this.props.card
+      cardCopy.name = this.props.card.name
+      cardCopy.description = this.state.description
+      cardCopy.label = this.state.label
+      cardCopy.dueDate = this.state.dueDate
+      this.props.updateCard(cardCopy, false)
+      this.props.toggleHidden()
   }
 
   render() {
@@ -19,15 +58,15 @@ class CardDetail extends React.Component {
           <div className="contentContainer">
             <div className="cardNameContainer">
               <p>Name</p>
-              <input/>
+              <input value={this.props.card.name} onChange={this.editingName}/>
             </div>
             <div className="cardDescriptionContainer">
               <p>Description</p>
-              <textarea></textarea>
+              <textarea value={this.state.description} onChange={this.editingDescription}></textarea>
             </div>
             <div className="dueDateContainer">
               <p>Due-Date</p>
-              <input/>
+              <input value={this.state.dueDate} onChange={this.editingDueDate}/>
             </div>
             <div className="cardLabelContainer">
               <p>Label</p>
@@ -39,7 +78,7 @@ class CardDetail extends React.Component {
             </div>
           </div>
           <div className="saveChanges">
-            <button title="Save all the changes made to this card">SAVE</button>
+            <button title="Save all the changes made to this card" onClick={this.finishEditing}>SAVE</button>
           </div>
         </div>
       </div>
