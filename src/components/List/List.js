3b87d65cb183871eca481c277f674f058adb87ab
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-15 15:14:01 +0530

    Added methods to handle cards in state

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './List.css'
 import Card from '../Card/Card.js'
+import shortid from 'shortid'
 
 class List extends React.Component {
   constructor(props) {
@@ -15,6 +16,7 @@ class List extends React.Component {
     this.toggleDisable = this.toggleDisable.bind(this)
     this.editing = this.editing.bind(this)
     this.finishEditing = this.finishEditing.bind(this)
+    this.updateCards = this.updateCards.bind(this)
   }
 
   deleteThisList() {
@@ -22,11 +24,9 @@ class List extends React.Component {
   }
 
   toggleDisable() {
-    console.log('In the toggle disp')
     this.setState({
       isDisabled : !this.state.isDisabled
     })
-    console.log(this.state.isDisabled)
   }
 
   editing(event) {
@@ -42,19 +42,38 @@ class List extends React.Component {
   }
 
   addNewCard() {
+    let cardsCopy = this.state.cards
+    cardsCopy.push({name: '', label: 'white', description: '', dueDate: '', cardId: shortid.generate()})
+    this.setState({
+      cards : cardsCopy
+    })
+  }
 
+  updateCards(card, toRemove = true) {
+    let cardsCopy = this.state.cards
+    for( let item of cardsCopy) {
+      if(item.cardId === card.cardId) {
+        if(toRemove)
+        cardsCopy.splice(cardsCopy.indexOf(item), 1)
+        else item = card
+        break;
+      }
+    }
+    this.setState({
+      cards : cardsCopy
+    })
   }
 
   render() {
     const allCards = this.state.cards.map( (card) => {
-      return <Card/>
+      return <Card key={card.cardId} card={card} updateCards={this.updateCards}/>
     })
     return (
       <div className="listContainer">
         <div className="deleteThisList" title="Delete this entire List" onClick={this.deleteThisList}>&times;</div>
         <div className="listNameHolder">
-        <input value={this.state.name} disabled={!this.state.isDisabled} onChange={this.editing} onKeyUp={this.finishEditing}/>
-        <button className="toggleDisableButton" title="EDIT List Name" onClick={this.toggleDisable}>&#x270E;</button>
+          <input value={this.state.name} disabled={!this.state.isDisabled} onChange={this.editing} onKeyUp={this.finishEditing}/>
+          <button className="toggleDisableButton" title="EDIT List Name" onClick={this.toggleDisable}>&#x270E;</button>
         </div>
         <div className="cardComponents">
           {allCards}
