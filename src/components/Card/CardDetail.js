b7bac168a522279c5e2fd567651ce892678213da
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2018-11-15 17:51:22 +0530

    Used props, added a method and structure the render template

--- src/components/Card/CardDetail.js
+++ src/components/Card/CardDetail.js
@@ -2,27 +2,35 @@ import React from 'react'
 import './CardDetail.css'
 
 class CardDetail extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.toggleDisplay = this.toggleDisplay.bind(this)
+  }
+
+  toggleDisplay() {
+    this.props.toggleHidden()
   }
 
   render() {
     return (
-      <div className="cardModalWindowBG">
+      <div className="cardModalWindowBG" style={{display : this.props.isHidden}}>
         <div className="cardModalWindow">
-          <div className="closeIt">&times;</div>
-          <div className="cardNameContainer">
-            <p>Name</p>
-            <input/>
-            <button>S</button>
-          </div>
-          <div className="cardDescriptionContainer">
-            <p>Description</p>
-            <textarea></textarea>
-            <button>S</button>
-          </div>
-          <div className="cardLabelContainer">
-            <p>Label</p>
+          <div className="closeIt" onClick={this.toggleDisplay}>&times;</div>
+          <div className="contentContainer">
+            <div className="cardNameContainer">
+              <p>Name</p>
+              <input/>
+              </div>
+              <div className="cardDescriptionContainer">
+              <p>Description</p>
+              <textarea></textarea>
+              </div>
+              <div className="cardLabelContainer">
+              <p>Label</p>
+              </div>
+            </div>
+          <div className="saveChanges">
+            <button title="Save all the changes made to this card">SAVE</button>
           </div>
         </div>
       </div>
